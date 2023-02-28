bc2661058d8a237dbc5316e6baca17eca10977a6
ryhhill1998 <ryhhill1998@outlook.com>
2023-02-28 11:04:00 +0000

    Added functionality to show selected dropdown

--- src/Components/Navbar/CollapsedNav/CollapsedNav.component.jsx
+++ src/Components/Navbar/CollapsedNav/CollapsedNav.component.jsx
@@ -2,30 +2,35 @@ import {faCircleChevronDown} from "@fortawesome/free-solid-svg-icons";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 
 import "./CollapsedNav.styles.css";
+import {Link} from "react-router-dom";
 
 const CollapsedNav = () => {
+    const toggleDropdown = () => {
+        document.getElementById("dropdown-menu").classList.toggle("no-display");
+    }
+
     return (
         <>
             <nav className="collapsed-nav">
                 <ul>
-                    <li className="dropdown-button" id="hamburger">
-                        Menu <FontAwesomeIcon icon={faCircleChevronDown} className="icon dropdown-arrow" />
+                    <li className="dropdown-button" id="hamburger" onClick={toggleDropdown}>
+                        Menu <FontAwesomeIcon icon={faCircleChevronDown} className="icon dropdown-arrow"/>
                     </li>
                 </ul>
             </nav>
             <div id="dropdown-menu" className="collapsed-nav-dropdown no-display">
                 <ul>
-                    <li><a className="dropdown-selected" href="BasicLayout/CollapsedNav">Home</a></li>
-                    <li><a href="src/components">Sports Massage</a></li>
-                    <li><a href="src/components">Reflexology</a></li>
-                    <li><a href="src/components">Swedish Massage</a></li>
-                    <li><a href="src/components">Indian Head Massage</a></li>
-                    <li><a href="src/components">LA Stone Therapy</a></li>
-                    <li><a href="src/components">Corporate Pamper Days</a></li>
-                    <li><a href="src/components">Peer-to-Peer Massage</a></li>
-                    <li><a href="src/components">FAQs</a></li>
-                    <li><a href="src/components#section--testimonials">Testimonials</a></li>
-                    <li><a href="src/components#top" className="contact-button">Contact</a></li>
+                    <li><Link to={"/"} className="dropdown-selected">Home</Link></li>
+                    <li><Link to={"/sports-massage"}>Sports Massage</Link></li>
+                    <li><Link to={"/reflexology"}>Reflexology</Link></li>
+                    <li><Link to={"/swedish-massage"}>Swedish Massage</Link></li>
+                    <li><Link to={"/indian-head-massage"}>Indian Head Massage</Link></li>
+                    <li><Link to={"/la-stone-therapy"}>LA Stone Therapy</Link></li>
+                    <li><Link to={"/corporate-pamper-days"}>Corporate Pamper Days</Link></li>
+                    <li><Link to={"/peer-to-peer"}>Peer-to-Peer Massage</Link></li>
+                    <li><Link to={"/"}>FAQs</Link></li>
+                    <li><Link to={"/"}>Testimonials</Link></li>
+                    <li><Link to={"/"} className="contact-button">Contact</Link></li>
                 </ul>
             </div>
         </>
--- src/Components/Navbar/MainNav/MainNav.component.jsx
+++ src/Components/Navbar/MainNav/MainNav.component.jsx
@@ -7,6 +7,16 @@ import "./MainNav.styles.css";
 
 const MainNav = () => {
 
+    const showActiveLink = ({target}) => {
+        const allLinks = document.querySelectorAll("#services-dropdown a");
+
+        allLinks.forEach(link => {
+            link.classList.remove("dropdown-selected");
+        });
+
+        target.classList.add("dropdown-selected");
+    }
+
     const toggleServices = () => {
         document.getElementById("services-dropdown").classList.toggle("no-display");
     }
@@ -20,26 +30,75 @@ const MainNav = () => {
             <ul>
                 <li><Link to={`/`}>Home</Link></li>
                 <li className="services" onMouseLeave={toggleServices}>
-                    <Link to={""} className="dropdown-button" id="services-button" onClick={toggleServices}
+                    <a className="dropdown-button" id="services-button" onClick={toggleServices}
                        onMouseEnter={showServices}>
                         Services
                         <FontAwesomeIcon icon={faCircleChevronDown} className="icon dropdown-arrow"/>
-                    </Link>
+                    </a>
                     <div id="services-dropdown" className="no-display">
                         <ul>
-                            <li><Link to={`/sports-massage`} href="src/components">Sports Massage</Link></li>
-                            <li><Link to={`/reflexology`} href="src/components">Reflexology</Link></li>
-                            <li><Link to={`/swedish-massage`} href="src/components">Swedish Massage</Link></li>
-                            <li><Link to={`/indian-head-massage`} href="src/components">Indian Head Massage</Link></li>
-                            <li><Link to={`/la-stone-therapy`} href="src/components">LA Stone Therapy</Link></li>
-                            <li><Link to={`/corporate-pamper-days`} href="src/components">Corporate Pamper Days</Link></li>
-                            <li><Link to={`/peer-to-peer`} href="src/components">Peer-to-Peer Massage</Link></li>
+                            <li>
+                                <Link
+                                    to={`/sports-massage`}
+                                    onClick={showActiveLink}>
+                                    Sports Massage
+                                </Link>
+                            </li>
+                            <li>
+                                <Link
+                                    to={`/reflexology`}
+                                    onClick={showActiveLink}>
+                                    Reflexology
+                                </Link>
+                            </li>
+                            <li>
+                                <Link
+                                    to={`/swedish-massage`}
+                                    onClick={showActiveLink}>
+                                    Swedish Massage
+                                </Link>
+                            </li>
+                            <li>
+                                <Link
+                                    to={`/indian-head-massage`}
+                                    onClick={showActiveLink}>
+                                    Indian Head Massage
+                                </Link>
+                            </li>
+                            <li>
+                                <Link
+                                    to={`/la-stone-therapy`}
+                                    onClick={showActiveLink}>
+                                    LA Stone Therapy
+                                </Link>
+                            </li>
+                            <li>
+                                <Link
+                                    to={`/acupuncture`}
+                                    onClick={showActiveLink}>
+                                    Acupuncture
+                                </Link>
+                            </li>
+                            <li>
+                                <Link
+                                    to={`/corporate-pamper-days`}
+                                    onClick={showActiveLink}>
+                                    Corporate Pamper Days
+                                </Link>
+                            </li>
+                            <li>
+                                <Link
+                                    to={`/peer-to-peer`}
+                                    onClick={showActiveLink}>
+                                    Peer-to-Peer Massage
+                                </Link>
+                            </li>
                         </ul>
                     </div>
                 </li>
-                <li><a href="src/components">FAQs</a></li>
-                <li><a href="src/components#section--testimonials">Testimonials</a></li>
-                <li><a href="src/components#top" className="contact-button">Contact</a></li>
+                <li><Link to={""}>FAQs</Link></li>
+                <li><a href="#section--testimonials">Testimonials</a></li>
+                <li><Link to={""} className="contact-button">Contact</Link></li>
             </ul>
         </nav>
     );
