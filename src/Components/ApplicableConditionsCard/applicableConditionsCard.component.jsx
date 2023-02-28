3d55c50767f91b08cac755db9d37291f8b12a555
ryhhill1998 <ryhhill1998@outlook.com>
2023-02-28 17:01:54 +0000

    Started adding applicable conditions card to pages

---
+++ src/Components/ApplicableConditionsCard/applicableConditionsCard.component.jsx
@@ -0,0 +1,22 @@
+import "./applicableCondtionsCard.styles.css";
+
+const ApplicableConditionsCard = () => {
+    return (
+        <div className="conditions-card">
+            <h2>Applicable Conditions</h2>
+            <ul className="bullet-list">
+                <li>Hormonal imbalances</li>
+                <li>Menopause</li>
+                <li>Back pain</li>
+                <li>Migraines, headaches</li>
+                <li>Sleep disorders</li>
+                <li>Digestive problems</li>
+                <li>Circulatory problems</li>
+                <li>Stress-related disorders</li>
+                <li>Arthritis</li>
+            </ul>
+        </div>
+    );
+}
+
+export default ApplicableConditionsCard;
--- src/Routes/Reflexology/reflexology.jsx
+++ src/Routes/Reflexology/reflexology.jsx
@@ -2,6 +2,8 @@ import "./reflexology.css";
 
 import reflexologyImageSrc from "../../Images/reflexology.jpeg";
 import About from "../../Components/AboutSection/About.component";
+import DottedSeparator from "../../Components/DottedSeparator/DottedSeparator.component";
+import ApplicableConditionsCard from "../../Components/ApplicableConditionsCard/applicableConditionsCard.component";
 
 const cardInfo = {
     heading: "Reflexology",
@@ -11,7 +13,9 @@ const cardInfo = {
         `It is based on a system of zones and reflex areas on the foot or hand that match areas of the body. 
         Manipulating these areas promotes self-healing and brings balance to the body.`,
         `Helping to strengthen the immune system it is a wonderfully relaxing treatment which also detoxifies and 
-        cleanses the body whilst relaxing muscles and relieving stress and tension.`
+        cleanses the body whilst relaxing muscles and relieving stress and tension.`,
+        `A treatment ensures every part of the foot or hand is worked on and a relaxing and soothing foot or hand 
+        massage concludes the treatment.`
     ],
     imageSrc: reflexologyImageSrc
 };
@@ -20,6 +24,8 @@ const Reflexology = () => {
     return (
         <main className="container">
             <About {...cardInfo} />
+            <DottedSeparator/>
+            <ApplicableConditionsCard/>
         </main>
     );
 }
