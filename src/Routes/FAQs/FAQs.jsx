93c1896ee3ff17bc62f7c4b5e6792f1e207716fa
ryhhill1998 <ryhhill1998@outlook.com>
2023-03-01 18:41:44 +0000

    Starting adding faqs page to site

--- src/Routes/FAQs/FAQs.jsx
+++ src/Routes/FAQs/FAQs.jsx
@@ -1,8 +1,60 @@
 import "./FAQs.css";
+import {faCircleChevronDown} from "@fortawesome/free-solid-svg-icons";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 
 const FAQs = () => {
     return (
-        <h1>FAQs</h1>
+        <main className="container">
+            <div className="faqs-container">
+                <div className="question-container">
+                    <h2 className="question">
+                        Who can have acupuncture treatment?
+                        <FontAwesomeIcon icon={faCircleChevronDown} className="icon dropdown-arrow"/>
+                    </h2>
+                    <p className="answer">
+                        There is no limitation in terms of age, sex or condition regarding acupuncture treatment. The very
+                        young or extremely old can be treated. All types of conditions can be treated – chronic or acute,
+                        severe or mild, mental or physical.
+                    </p>
+                </div>
+
+                <div className="question-container">
+                    <h2 className="question">
+                        Who can have acupuncture treatment?
+                        <FontAwesomeIcon icon={faCircleChevronDown} className="icon dropdown-arrow"/>
+                    </h2>
+                    <p className="answer">
+                        There is no limitation in terms of age, sex or condition regarding acupuncture treatment. The very
+                        young or extremely old can be treated. All types of conditions can be treated – chronic or acute,
+                        severe or mild, mental or physical.
+                    </p>
+                </div>
+
+                <div className="question-container">
+                    <h2 className="question">
+                        Who can have acupuncture treatment?
+                        <FontAwesomeIcon icon={faCircleChevronDown} className="icon dropdown-arrow"/>
+                    </h2>
+                    <p className="answer">
+                        There is no limitation in terms of age, sex or condition regarding acupuncture treatment. The very
+                        young or extremely old can be treated. All types of conditions can be treated – chronic or acute,
+                        severe or mild, mental or physical.
+                    </p>
+                </div>
+
+                <div className="question-container">
+                    <h2 className="question">
+                        Who can have acupuncture treatment?
+                        <FontAwesomeIcon icon={faCircleChevronDown} className="icon dropdown-arrow"/>
+                    </h2>
+                    <p className="answer">
+                        There is no limitation in terms of age, sex or condition regarding acupuncture treatment. The very
+                        young or extremely old can be treated. All types of conditions can be treated – chronic or acute,
+                        severe or mild, mental or physical.
+                    </p>
+                </div>
+            </div>
+        </main>
     );
 }
 
