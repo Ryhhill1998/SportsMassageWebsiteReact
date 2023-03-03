c8956f56981c81cbbc1c305cb6d40c1e76ba4809
ryhhill1998 <ryhhill1998@outlook.com>
2023-03-03 08:14:16 +0000

    Adjusted dropdown to be 65vh

--- src/Components/Navbar/CollapsedNav/CollapsedNav.component.jsx
+++ src/Components/Navbar/CollapsedNav/CollapsedNav.component.jsx
@@ -5,8 +5,12 @@ import "./CollapsedNav.styles.css";
 import {Link} from "react-router-dom";
 import {navigationLinks} from "../MainNav/MainNav.component";
 import NavigationLink from "../NavigationLink/NavigationLink.component";
+import {useState} from "react";
+import SearchBox from "../../SearchBox/SearchBox.component";
 
 const CollapsedNav = () => {
+
+    const [dropdownSearch, setDropdownSearch] = useState(false);
 
     const toggleDropdown = () => {
         document.getElementById("dropdown-menu").classList.toggle("no-display");
@@ -16,16 +20,23 @@ const CollapsedNav = () => {
         document.getElementById("dropdown-menu").classList.add("no-display");
     }
 
+    const dropdownSearchBox = () => {
+
+    }
+
     return (
         <>
             <nav className="collapsed-nav">
                 <ul>
-                    <li><FontAwesomeIcon icon={faMagnifyingGlass} className="icon" /></li>
+                    <li className="open-search-button">
+                        <FontAwesomeIcon icon={faMagnifyingGlass} className="icon" />
+                    </li>
                     <li className="dropdown-button" id="hamburger" onClick={toggleDropdown}>
                         Menu <FontAwesomeIcon icon={faCircleChevronDown} className="icon dropdown-arrow"/>
                     </li>
                 </ul>
             </nav>
+            {/*<SearchBox/>*/}
             <div id="dropdown-menu" className="collapsed-nav-dropdown no-display">
                 <ul>
                     <NavigationLink text="Home" route="/" onClickHandler={hideDropdown} />
